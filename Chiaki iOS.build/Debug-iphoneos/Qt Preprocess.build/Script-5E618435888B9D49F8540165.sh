ee565e494d6b58d1419bd922898c1c4363269d9d
kvtas <jebacmpk@gmail.com>
2026-09-05 21:02:45 +0200

    Update Script-5E618435888B9D49F8540165.sh

--- Chiaki iOS.build/Debug-iphoneos/Qt Preprocess.build/Script-5E618435888B9D49F8540165.sh
+++ Chiaki iOS.build/Debug-iphoneos/Qt Preprocess.build/Script-5E618435888B9D49F8540165.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-set -e
-
-SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
-PROJECT_DIR="$(CDPATH= cd -- "$SCRIPT_DIR/.." && pwd)"
-
-make -C "$PROJECT_DIR" -f "$PROJECT_DIR/Chiaki iOS.xcodeproj/qt_makeqmake.mak"
+make -C . -f chiaki\ iOS.xcodeproj/qt_makeqmake.mak
